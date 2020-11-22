f5582a6ea5de5608f332237a8e58d3c740fecfe7
huouvcti <huouvcti@gmail.com>
2020-11-22 23:24:54 +0900

    like

--- melon.py
+++ melon.py
@@ -14,10 +14,13 @@ soup = bs(html, "html.parser")
 rank_list = soup.find_all("span", {"class": "rank"})
 title_list = soup.find_all("div", {"class": "ellipsis rank01"})
 singer_list = soup.find_all("span", {"class": "checkEllipsis"})
+like_list = soup.find_all("button", {"class": "button_etc like"})
 
 data = []
 for i in range(100):
-    data.append([i+1, title_list[i].text.replace("\n",""), singer_list[i].text.replace("\n","")])
+    title = title_list[i].text.replace("\n","")
+    like = like_list[i].text.replace("\n", "").replace("좋아요", "").replace("총건수", "")
+    singer = singer_list[i].text.replace("\n","")
+    data.append([i+1, title, like, singer])
     print(str(data[i][0])+"위", data[i][1], data[i][2])
 
-print(data)
